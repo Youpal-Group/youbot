4d627b497f817b3eee498998657217f560340362
Vassilis <bkrith@hotmail.com>
2021-02-17 16:04:25 +0200

    All direct commands starts with dot (.)

--- scripts/commands/index.js
+++ scripts/commands/index.js
@@ -5,13 +5,22 @@ module.exports = {
 	name: 'commands',
 	outgoing: true,
 	script: async (event, bot) => {
-		return bot.hear('^//(.*?) (.*)', async (regParts) => {
-			if (regParts[1] && regParts[2]) {
-				await bot.script('command-' + regParts[1]).script(event, bot, regParts[2]);
-				bot.logger.info('Commands', `Outgoing - Executed command-${regParts[1]}`);
-			}
+		return bot.hear(/^\.(.*?) (.*)/, async (regParts) => {
+			try {
+				if (regParts[1] && regParts[2]) {
+					await bot.script('command-' + regParts[1]).script(event, bot, regParts[2]);
+					bot.logger.info('Commands', `Outgoing - Executed command-${regParts[1]}`);
 
-			return false;
+					return false;
+				}
+
+				return true;
+			}
+			catch(err) {
+				bot.logger.error('Commands', err);
+
+				return true;
+			}
 		});
 	}
 };
